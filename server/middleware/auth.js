c25710a9f38f0d80fd6853a5e282e21a3e9b89a0
Abhishek <naviapawar@gmail.com>
2026-04-09 23:21:55 +0530

    Modified Auth.js file and make Message Conroller

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,28 +2,28 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
 
-
-  
-    try {
-
-      const token = req.headers.token;
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      const user = await User.findById(decoded.userId).select("-password");
-
-      if(!user) return res.json({success:false , message : "User mot found" });
-
-      req.user = user;
-
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401).json({ message: "Not authorized" });
+    // ✅ check token here (INSIDE function)
+    if (!token) {
+      return res.status(401).json({ message: "No token" });
     }
-  }
 
-  if (!token) {
-    res.status(401).json({ message: "No token" });
-  };
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "Not authorized" });
+  }
+};
---
+++ server/models/Message.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    // optional: for images / files
+    image: {
+      type: String,
+    },
+    seen: {
+      type:Boolean,
+      default: false,
+    }
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
